c86f174bf0127fb00886e7255f81dcb37c6d73b7
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-08-21 14:47:03 -0400

    requires written

---
+++ index.js
@@ -0,0 +1,9 @@
+const HtmlFile = require('.src/HtmlFile');
+
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const Engineer = require('./lib/Engineer');
+const intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
