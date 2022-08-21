5c35bd6b103b4375456cddf3227b554fb34d330a
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-08-21 18:57:30 -0400

    all test pass

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,3 +1,6 @@
 const Manager = require('../lib/Manager');
 
-test("")
+test("testing officenumber input", () => {
+    const numInput = new Manager("johnDoe", 14, "johnDoe@aol.com", 8675309)
+    expect(numInput.getOfficeNumber()).toEqual(8675309)
+});
